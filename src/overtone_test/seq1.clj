2a3031982a6c5b1addca52bfcb17d855a04b600c
AndrewJ <web@alphajuliet.com>
2015-10-14 21:13:21 +1100

    Add different beat options to basic sequencing in seq1.clj. Create a live sequencing example using code from an Overtone tutorial.

--- src/overtone_test/seq1.clj
+++ src/overtone_test/seq1.clj
@@ -1,4 +1,4 @@
-; Try some basic sequencing in Overtone
+; Try some basic drum sequencing in Overtone
 ; AJ 2015-10-10
 
 (ns overtone-test.core
@@ -29,28 +29,37 @@
 ; Stop it
 (stop)
 
-;; Something a little more sophisticated
+;; Now, for something a little more sophisticated
+
 (defn play-pattern [curr-t sep-t sound pattern]
   "Play a sound according to a binary pattern at a regular interval."
   (at curr-t (when (= 1 (first pattern)) (sound)))
   (let [new-t (+ curr-t sep-t)]
     (apply-by new-t #'play-pattern [new-t sep-t sound (rest pattern)])))
 
-(def inc-t 250) ; ms
+(def tick 250) ; ms
 (def p1 [1 0 1 0 1 0 1 0])
-(play-pattern (now) inc-t bd808 (cycle p1))
+(play-pattern (now) tick bd808 (cycle p1))
 (stop)
 
 ;; And several at once, in sync
-(def patterns {bd808 [1 0 1 0 1 0 1 0]
-               sd808 [0 0 1 0 0 0 1 0]
-               ch808 [0 1 0 1 0 1 0 0]
-               oh808 [0 0 0 0 0 0 0 1]})
+(def beat1 {bd808 [1 0]
+            sd808 [0 0 1 0]
+            ch808 [0 1]
+            oh808 [0 0 0 0 0 0 0 1]})
+
+(def beat2 {bd808 [1 0 0 0 0 1 0 0]
+            sd808 [0 0 1 0 0 0 1 0]
+            ch808 [1 1 1 1 1 1 1 0]
+            oh808 [0 0 0 0 0 0 0 1]})
 
 (defn play-patterns [sep-t patterns]
   (let [t (+ (now) sep-t)]
     (doseq [[sound pattern] patterns]
       (play-pattern t sep-t sound (cycle pattern)))))
 
-(play-patterns inc-t patterns)
+(play-patterns tick beat1)
+; or
+(play-patterns tick beat2)
+
 (stop)
---
+++ src/overtone_test/seq2.clj
@@ -0,0 +1,47 @@
+; Try some live sequencing
+; See https://github.com/overtone/overtone/blob/master/src/overtone/examples/getting_started/pragpub-article.clj
+; AJ 2015-10-14
+
+(ns overtone-test.core
+  (:use [overtone.live]
+        [overtone.inst.drum]))
+
+(def SAMPLES "/Volumes/Galatea 2TB/Music/Samples/")
+(defn my-sample [filename]
+  (sample (str SAMPLES filename)))
+
+(def bd808 (my-sample "drums/tr808/BD/BD0050.wav"))
+(def sd808 (my-sample "drums/tr808/SD/SD0050.wav"))
+(def ch808 (my-sample "drums/tr808/CH/CH.wav"))
+(def oh808 (my-sample "drums/tr808/OH/OH50.wav"))
+
+(def beat1 {dub-kick [1 0]
+            sd808 [0 0 1 0]
+            ch808 [0 1]
+            oh808 [0 0 0 0 0 0 0 1]})
+
+
+;; Make a live, editable pattern by using an atom for mutable state
+
+(def live-beats (atom beat1))
+
+(def tick 250) ; ms
+
+(defn live-sequencer
+  ([curr-t sep-t live-patterns] 
+   (live-sequencer curr-t sep-t live-patterns 0))
+  ([curr-t sep-t live-patterns beat]
+   (doseq [[sound pattern] @live-patterns
+           :when (= 1 (nth pattern (mod beat (count pattern))))]
+     (at curr-t (sound)))
+   (let [new-t (+ curr-t sep-t)]
+     (apply-by new-t #'live-sequencer [new-t sep-t live-patterns (inc beat)]))))
+
+(live-sequencer (now) tick live-beats)
+
+(swap! live-beats assoc dub-kick [1 0 1 0 1 0 1 0])
+(swap! live-beats assoc sd808 [0 0 1 0 0 0 1 1])
+(swap! live-beats assoc ch808 [0 1 1 1 0 1 1 0])
+
+(stop)
+
